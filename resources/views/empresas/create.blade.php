90831b78910c2b730207be4a861ad58b1b60f6b8
Sergio Rivoiro <srivoiro@gmail.com>
2025-02-03 17:33:01 -0300

    prueba de commit

--- resources/views/empresas/create.blade.php
+++ resources/views/empresas/create.blade.php
@@ -42,7 +42,23 @@
                 <input type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="Telefono">
 
                 <strong>Email:</strong>                
+                <input type="text" name="name" value="{{ old('name') }}" class="form-control" placeholder="CUIT">
+
+                <strong><div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">Title</h4>
+                        <h6 class="card-subtitle text-muted">Subtitle</h6>
+                    </div>
+                    <img src="holder.js/100x180/" alt="" />
+                    <div class="card-body">
+                        <p class="card-text">Text</p>
+                        <a href="#" class="card-link">Link 1</a>
+                        <a href="#" class="card-link">Link 2</a>
+                    </div>
+                </div>
+                :</strong>                
                 <input type="email" name="name" value="{{ old('name') }}" class="form-control" placeholder="Email">
+
 
                 
             </div>
