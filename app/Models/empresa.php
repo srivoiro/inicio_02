b3c07671d60695cf1e52e4ae2787af897ff0992e
Sergio Rivoiro <srivoiro@gmail.com>
2025-02-10 20:07:27 -0300

    desarrollo

---
+++ app/Models/empresa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class empresa extends Model
+{
+    use HasFactory;
+  
+    /**
+     * The attributes that are mass assignable.
+     *	
+     * @var array
+     */
+    protected $fillable = [
+        'razon_social',
+        'condicion_iva',
+        'cuit'
+    ];
+}
